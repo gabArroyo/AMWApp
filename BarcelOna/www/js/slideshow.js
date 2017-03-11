4c9d92db1ade23db76269344c05e11042976e52e
Gabi <gabristc23@gmail.com>
2017-03-11 03:27:35 +0100

    Nueva versión
    Subida nueva versión con algunas pantallas más. Todavía falta por
    conectar. Queda página de evento, lista apps (link a google y ya
    estaría), catálogo biblioteca, trámites, número de teléfono y hospital
    más cercano. Y añadir el código de todo junto.

--- BarcelOna/www/js/slideshow.js
+++ BarcelOna/www/js/slideshow.js
@@ -1,13 +1,17 @@
 var slideIndex = 1;
+var slideIndexMasRecomendadas = 1;
 
 showSlides(slideIndex);
+showSlidesMasRecomendadas(slideIndexMasRecomendadas);
+
+/* Slider del ayuntamiento */
 
 function resetSlideTimeout(){
   window.clearTimeout(timeoutHandle);
 }
 
 function plusSlides(n) {
-  resetSlideTimeout()
+  resetSlideTimeout();
   showSlides(slideIndex += n);
 }
 
@@ -16,14 +20,14 @@ function increaseSlides() {
 }
 
 function currentSlide(n) {
-  resetSlideTimeout()
+  resetSlideTimeout();
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("slidesAyuntamiento");
+  var dots = document.getElementsByClassName("dotAyuntamiento");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -34,4 +38,42 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  timeoutHandle = setTimeout(increaseSlides, 6000);
+}
+
+/* Slider apps más descargadas */
+
+function resetSlideTimeoutMasRecomendadas(){
+  window.clearTimeout(timeoutHandleMasRecomendadas);
+}
+
+function plusSlidesMasRecomendadas(n) {
+  resetSlideTimeoutMasRecomendadas();
+  showSlidesMasRecomendadas(slideIndexMasRecomendadas += n);
+}
+
+function increaseSlidesMasRecomendadas() {
+  showSlidesMasRecomendadas(slideIndexMasRecomendadas += 1);
+}
+
+function currentSlideMasRecomendadas(n) {
+  resetSlideTimeoutMasRecomendadas();
+  showSlidesMasRecomendadas(slideIndexMasRecomendadas = n);
+}
+
+function showSlidesMasRecomendadas(n) {
+  var i;
+  var slides = document.getElementsByClassName("slidesMasRecomendadas");
+  var dots = document.getElementsByClassName("dotMasRecomendadas");
+  if (n > slides.length) {slideIndexMasRecomendadas = 1}
+  if (n < 1) {slideIndexMasRecomendadas = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexMasRecomendadas-1].style.display = "block";
+  dots[slideIndexMasRecomendadas-1].className += " active";
+  timeoutHandleMasRecomendadas = setTimeout(increaseSlidesMasRecomendadas, 6000);
 }
